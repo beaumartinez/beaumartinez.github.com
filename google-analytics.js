06e5fa911a1c24d1c544e781832bf2350ac3ba80
Beau <beaumartinez@gmail.com>
2011-08-28 22:51:13 +0100

    Abstract Google Analytics code

---
+++ google-analytics.js
@@ -0,0 +1,6 @@
+var _gaq = [['_setAccount', 'UA-25375229-1'], ['_trackPageview']]
+
+var script = document.createElement('script')
+script.src = 'http://www.google-analytics.com/ga.js'
+
+document.appendChild(script)
