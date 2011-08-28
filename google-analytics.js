0f358a9c7e3515e0d3572cb4443670b2eb72a34b
Beau <beaumartinez@gmail.com>
2011-08-28 22:53:57 +0100

    Fix Google Analytics code

--- google-analytics.js
+++ google-analytics.js
@@ -3,4 +3,4 @@ var _gaq = [['_setAccount', 'UA-25375229-1'], ['_trackPageview']]
 var script = document.createElement('script')
 script.src = 'http://www.google-analytics.com/ga.js'
 
-document.appendChild(script)
+document.body.appendChild(script)
