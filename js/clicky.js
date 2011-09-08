c969e9c3d709449fe1ac8189eb44263984df5af9
Beau <beau@beaumartinez.com>
2011-09-08 23:02:24 +0100

    Use Clicky instead of Google Analytics

---
+++ js/clicky.js
@@ -0,0 +1,6 @@
+var clicky_site_ids = [66476050]
+
+var script = document.createElement('script')
+script.src = '//static.getclicky.com/js'
+
+document.body.appendChild(script)
--- js/google-analytics.js
+++
@@ -1,6 +0,0 @@
-var _gaq = [['_setAccount', 'UA-25375229-1'], ['_trackPageview']]
-
-var script = document.createElement('script')
-script.src = 'http://www.google-analytics.com/ga.js'
-
-document.body.appendChild(script)
