a2d990199e6719b93c9ebe1aa1e264c6fb30144f
Beau <beau@beaumartinez.com>
2011-09-14 18:05:00 +0100

    Use HTTP (not HTTP or HTTPS) for Clicky

--- js/clicky.js
+++ js/clicky.js
@@ -1,6 +1,6 @@
 var clicky_site_ids = [66476050]
 
 var script = document.createElement('script')
-script.src = '//static.getclicky.com/js'
+script.src = 'http://static.getclicky.com/js'
 
 document.body.appendChild(script)
