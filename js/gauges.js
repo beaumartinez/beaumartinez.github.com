2e4383eec51d280437bc134043466bfdf4820995
Beau <beau@beaumartinez.com>
2012-09-13 20:47:00 +0100

    Statement order

--- js/gauges.js
+++ js/gauges.js
@@ -1,7 +1,7 @@
 var _gauges = [];
 
 var script = document.createElement('script');
-script.src = '//secure.gaug.es/track.js';
 script.setAttribute('data-site-id', '5052348cf5a1f5508f000017');
+script.src = '//secure.gaug.es/track.js';
 
 document.body.appendChild(script);
