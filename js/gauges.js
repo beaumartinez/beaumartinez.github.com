9c231935614dcc3ebce7bfcc131debf55782e807
Beau <beau@beaumartinez.com>
2012-09-13 20:40:14 +0100

    Add Gauges JS

---
+++ js/gauges.js
@@ -0,0 +1,7 @@
+var _gauges = [];
+
+var script = document.createElement('script');
+script.src = '//secure.gaug.es/track.js';
+script.setAttribute('data-site-id', '5052348cf5a1f5508f000017');
+
+document.body.appendChild(script);
