049ce3b8eab8c8a42dc49c922ad3c4521c98f291
Beau <beau@beaumartinez.com>
2012-09-13 20:47:25 +0100

    Fix Gauges JS

--- js/gauges.js
+++ js/gauges.js
@@ -1,6 +1,7 @@
 var _gauges = [];
 
 var script = document.createElement('script');
+script.id = 'gauges-tracker';
 script.setAttribute('data-site-id', '5052348cf5a1f5508f000017');
 script.src = '//secure.gaug.es/track.js';
 
