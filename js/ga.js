6fd2cdb47e33dc9a9f60ed58a5f96bf40aeb90c0
Beau <beau@beaumartinez.com>
2012-09-14 13:23:31 +0100

    Add GA JS

---
+++ js/ga.js
@@ -0,0 +1,6 @@
+var _gaq = [['_setAccount', 'UA-25375229-1'], ['_trackPageview']];
+
+var script = document.createElement('script');
+script.src = '//google-analytics.com/ga.js';
+
+document.body.appendChild(script);
