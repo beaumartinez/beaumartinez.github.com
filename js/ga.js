49314b9345f8bd9a1d78285aea306deb211a292e
Beau <beau@beaumartinez.com>
2012-09-15 18:03:36 +0100

    Revert "Add GA JS"
    This reverts commit 6fd2cdb47e33dc9a9f60ed58a5f96bf40aeb90c0.

--- js/ga.js
+++
@@ -1,6 +0,0 @@
-var _gaq = [['_setAccount', 'UA-25375229-1'], ['_trackPageview']];
-
-var script = document.createElement('script');
-script.src = '//google-analytics.com/ga.js';
-
-document.body.appendChild(script);
